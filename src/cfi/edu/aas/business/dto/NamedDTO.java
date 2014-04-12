416d1c1ffde4442383a07ea5eaeca76647fe3cd7
Surya Kiran <suryakiranl@gmail.com>
2014-04-12 11:21:19 -0700

    Parent DTO classes - BaseDTO and NamedDTO

---
+++ src/cfi/edu/aas/business/dto/NamedDTO.java
@@ -0,0 +1,22 @@
+package cfi.edu.aas.business.dto;
+
+public class NamedDTO extends BaseDTO {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "NamedDTO [name=" + name + ", getId()=" + getId()
+				+ ", getModifiedBy()=" + getModifiedBy()
+				+ ", getModifiedTime()=" + getModifiedTime()
+				+ ", getCreatedBy()=" + getCreatedBy() + ", getCreatedTime()="
+				+ getCreatedTime() + "]";
+	}
+}
