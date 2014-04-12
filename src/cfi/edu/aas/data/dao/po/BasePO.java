b90887da5bee1e42b3ca2733930acfaf014d9160
Surya Kiran <suryakiranl@gmail.com>
2014-04-12 11:21:37 -0700

    Parent PO classes - BasePO and NamedPO

---
+++ src/cfi/edu/aas/data/dao/po/BasePO.java
@@ -0,0 +1,68 @@
+package cfi.edu.aas.data.dao.po;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Entity
+@MappedSuperclass
+public class BasePO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String modifiedBy;
+	private Timestamp modifiedTime;
+	private String createdBy;
+	private Timestamp createdTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Timestamp getModifiedTime() {
+		return modifiedTime;
+	}
+
+	public void setModifiedTime(Timestamp modifiedTime) {
+		this.modifiedTime = modifiedTime;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Timestamp createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	@Override
+	public String toString() {
+		return "BasePO [id=" + id + ", modifiedBy=" + modifiedBy
+				+ ", modifiedTime=" + modifiedTime + ", createdBy=" + createdBy
+				+ ", createdTime=" + createdTime + "]";
+	}
+}
---
+++ src/cfi/edu/aas/data/dao/po/NamedPO.java
@@ -0,0 +1,27 @@
+package cfi.edu.aas.data.dao.po;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@Entity
+@MappedSuperclass
+public class NamedPO extends BasePO {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "NamedPO [name=" + name + ", getId()=" + getId()
+				+ ", getModifiedBy()=" + getModifiedBy()
+				+ ", getModifiedTime()=" + getModifiedTime()
+				+ ", getCreatedBy()=" + getCreatedBy() + ", getCreatedTime()="
+				+ getCreatedTime() + "]";
+	}
+}
