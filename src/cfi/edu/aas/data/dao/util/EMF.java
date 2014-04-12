473c26f7eb580b7b27419dd98af9648c9f61c67c
Surya Kiran <suryakiranl@gmail.com>
2014-04-12 11:22:53 -0700

    EntityMangerFactory to connect to database
    This connects to Google App Engine database to save and retrieve
    information

---
+++ src/cfi/edu/aas/data/dao/util/EMF.java
@@ -0,0 +1,16 @@
+package cfi.edu.aas.data.dao.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public final class EMF {
+	private static final String EMF_NAME = "out-of-office-web";
+    private static final EntityManagerFactory emfInstance =
+        Persistence.createEntityManagerFactory(EMF_NAME);
+
+    private EMF() {}
+
+    public static EntityManagerFactory get() {
+        return emfInstance;
+    }
+}
